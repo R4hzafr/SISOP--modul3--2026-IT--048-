42f312adb9c091122bb7a3d250c0aeb3a1c5c707
R4hzafr <azharroisul05@gmail.com>
2026-04-29 18:07:23 +0700

    Struktur final bersih sesuai aturan asdos

---
+++ sisop3/soal1/protocol.h
@@ -0,0 +1,17 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <time.h>
+
+// Definisi port dan kapasitas
+#define PORT 8080
+#define BUFFER_SIZE 1024
+#define MAX_CLIENTS 10
+
+#endif
